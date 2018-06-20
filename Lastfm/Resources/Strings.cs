502a51f30a1312f236e0ea8d171fd85bedcbc30b
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-06-20 14:21:11 -0400

    add scrobbler, but still not building yet

---
+++ Lastfm/Resources/Strings.cs
@@ -0,0 +1,27 @@
+﻿namespace Lastfm.Resources
+{
+    public static class Strings
+    {
+        public static class Endpoints
+        {
+            public static string LastfmApi  = "ws.audioscrobbler.com";
+        }
+
+        public static class Methods
+        {
+            public static string Scrobble         = "track.scrobble";
+            public static string NowPlaying       = "track.updateNowPlaying";
+            public static string GetMobileSession = "auth.getMobileSession";
+            public static string TrackLove        = "track.love";
+            public static string TrackUnlove      = "track.unlove";
+            public static string GetLovedTracks   = "user.getLovedTracks";
+            public static string GetTracks        = "library.getTracks";
+        }
+
+        public static class Keys
+        {
+            public static string LastfmApiKey     = "LASTFM_API_KEY_REMOVED";
+            public static string LastfmApiSeceret = "LASTFM_API_SECRET_REMOVED";
+        }
+    }
+}
